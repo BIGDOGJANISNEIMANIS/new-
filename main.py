acefe243cd13307adcbca10913f91b40739ff12e
BIGDOGJANISNEIMANIS <janis.lavits@gmail.com>
2024-03-20 14:25:35 +0200

    oh yea

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "sveiki"
+    return render_template("index.html")
+
+@app.route("/teksts")
+def te():
+    return render_template("freak.html")
+
 
 if __name__ == '__main__':
     app.run(port= 5000)
